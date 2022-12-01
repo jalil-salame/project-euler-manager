d890de0c673924956e3e8b8c72885b2a5b97dc9a
Jalil David Salamé Messina <jalil.salame@tum.de>
2022-12-01 23:33:31 +0100

    feat: Use RON instead of TOML

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::{Parser, Subcommand};
 
 mod parser;
 
-use crate::parser::{parse_problems, ProblemID};
+use crate::parser::{parse_problems, Problem, ProblemID};
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -38,9 +38,7 @@ fn main() {
         Command::Generate => {
             let (_rest, problems) = parse_problems(OFFLINE_PROBLEMS).expect("successful parse");
             assert_eq!("", _rest);
-            std::fs::write("problems.toml", toml::to_string(&problems).unwrap()).unwrap();
-
-            // toml::to_string(&problems).unwrap();
+            std::fs::write("problems.ron", ron::to_string(&problems).unwrap()).unwrap();
         }
         Command::Run { solution, time } => todo!("run {solution:?} {time}"),
         Command::Create { solution } => todo!("create {solution}"),
