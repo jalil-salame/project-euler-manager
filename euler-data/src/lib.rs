960144457e3dbbad7ccb3d188657fd50eb917d47
Jalil David Salamé Messina <jalil.salame@tum.de>
2022-12-03 01:36:11 +0100

    feat: Moved template to euler-data

--- euler-data/src/lib.rs
+++ euler-data/src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate lazy_static;
+use hex::FromHex;
 
 mod parser;
 use crate::parser::parse_problems;
@@ -11,4 +12,53 @@ lazy_static! {
     pub static ref PROBLEMS: Vec<Problem<'static>> = parse_problems(OFFLINE_PROBLEMS_STR)
         .expect("successful parse")
         .1;
+}
+
+pub fn find_problem(id: ProblemID) -> Option<Problem<'static>> {
+    PROBLEMS.iter().find(|problem| problem.id == id).cloned()
+}
+
+pub fn rust_template(id: ProblemID) -> String {
+    let problem = find_problem(id);
+
+    let mut description_str =
+        "Note: this problem is missing from the database, consider contributing to it".to_string();
+    let mut links_str = "\n".to_string();
+    let mut md5_hash = None;
+    let mut hash_comment = "is missing, consider contributing it";
+
+    if let Some(Problem {
+        id: _,
+        description,
+        links,
+        hash,
+    }) = problem
+    {
+        description_str = description.join("\n//");
+        if !links.is_empty() {
+            links_str = format!("// Visible Links\n//   {}\n", links.join("\n//   "));
+        }
+
+        md5_hash = hash.map(|s| <[u8; 16]>::from_hex(s).expect("valid hex string"));
+
+        if hash.is_some() {
+            hash_comment = "";
+        }
+    }
+
+    format!(
+        "// Project Euler: Problem {id}
+//
+// {description_str}
+{links_str}
+/// The MD5 hash of the answer to Problem {id}{hash_comment}
+pub static EXPECTED_ANSWER_HASH: Option<[u8; 16]> = {md5_hash:?};
+
+/// Solution to Problem {id}
+///
+/// NOTE: Auto-generated, don't change signature
+pub fn solution() -> impl Display {{
+    0
+}}"
+    )
 }
--- euler-manager/src/main.rs
+++ euler-manager/src/main.rs
@@ -1,8 +1,7 @@
 use std::path::Path;
 
 use clap::Parser;
-use hex::FromHex;
-use project_euler_data::{Problem, ProblemID, PROBLEMS};
+use project_euler_data::{rust_template, ProblemID};
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -16,8 +15,7 @@ struct Cli {
 fn main() {
     let Cli { solution, force } = Cli::parse();
 
-    let problem = PROBLEMS.iter().find(|p| p.id == solution);
-    let template = rust_template(problem.cloned(), solution);
+    let template = rust_template(solution);
 
     let file = Path::new("src")
         .join("solution")
@@ -28,47 +26,4 @@ fn main() {
     }
 
     std::fs::write(&file, template).expect("write template");
-}
-
-fn rust_template(problem: Option<Problem>, id: ProblemID) -> String {
-    let mut description_str =
-        "Note: this problem is missing from the database, consider contributing to it".to_string();
-    let mut links_str = "\n".to_string();
-    let mut md5_hash = None;
-    let mut hash_comment = "is missing, consider contributing it";
-
-    if let Some(Problem {
-        id: _,
-        description,
-        links,
-        hash,
-    }) = problem
-    {
-        description_str = description.join("\n//");
-        if !links.is_empty() {
-            links_str = format!("// Visible Links\n//   {}\n", links.join("\n//   "));
-        }
-
-        md5_hash = hash.map(|s| <[u8; 16]>::from_hex(s).expect("valid hex string"));
-
-        if hash.is_some() {
-            hash_comment = "";
-        }
-    }
-
-    format!(
-        "// Project Euler: Problem {id}
-//
-// {description_str}
-{links_str}
-/// The MD5 hash of the answer to Problem {id}{hash_comment}
-pub static EXPECTED_ANSWER_HASH: Option<[u8; 16]> = {md5_hash:?};
-
-/// Solution to Problem {id}
-///
-/// NOTE: Auto-generated, don't change signature
-pub fn solution() -> impl Display {{
-    0
-}}"
-    )
 }
